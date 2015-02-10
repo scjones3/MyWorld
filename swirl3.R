d8bd595bd6e5fdde5b393c28d0275238ada01bb2
Shaq Jones <scjones3@MYSENECA.CA>
2015-02-10 13:21:27 -0500

    swirl

---
+++ swirl3.R
@@ -0,0 +1,15 @@
+library("swirl")
+swirl()
+num_vect <- c(0.5, 55, -10, 6)
+tf <- num_vect < 1
+tf
+num_vect >= 6
+my_char <- c("My","name","is")
+my_char
+my_name <- c(my_char, "Ajay")
+my_name
+paste(my_name, collapse = " ")
+paste("Hello", "world!", sep = " ")
+paste("Hello", "world!", sep = " ")
+paste(1:3, c("X","Y","Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
